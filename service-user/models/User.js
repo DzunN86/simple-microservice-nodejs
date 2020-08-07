e6f7ff26e40d9f92c1cc8d4973eb9cd6bd004d96
DzunN86 <nurroinz@gmail.com>
2020-08-07 10:37:06 +0700

    create User model

---
+++ service-user/models/User.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes,
+            allowNull: false
+        },
+        role: {
+            type: DataTypes.ENUM,
+            values: ['admin', 'student'],
+            allowNull: false,
+            defaultValues: 'student'
+        },
+        avatar: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        profession: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        created_at: {
+            field: 'created_at',
+            type: DataTypes.DATE
+        },
+        updated_at: {
+            field: 'updated_at',
+            type: DataTypes.DATE
+        }
+
+    }, {
+        tebleName: 'users',
+        timeStamps: true
+    });
+
+    return User;
+}
