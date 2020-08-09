e6765178d9cd3dd481c6baf8e13e45514ffe76ca
DzunN86 <nurroinz@gmail.com>
2020-08-09 11:58:01 +0700

    create api filter get by id

--- service-user/routes/handler/users/getUsers.js
+++ service-user/routes/handler/users/getUsers.js
@@ -2,10 +2,19 @@ const { User } = require('../../../models');
 
 module.exports = async(req, res) => {
 
-    
-    const users = await User.findAll({
+    const userIds = req.query.user_ids || [];
+
+    const sqlOPtions = {
         attributes: ['id', 'name', 'email', 'role', 'profession', 'avatar']
-    });
+    }
+
+    if (userIds.length) {
+        sqlOPtions.where = {
+            id: userIds
+        }
+    }
+
+    const users = await User.findAll(sqlOPtions);
 
     return res.json({
         status: 'success',
