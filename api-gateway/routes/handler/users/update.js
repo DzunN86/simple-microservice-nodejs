38168dfdbc4a4ea1af2778c0e982b4b4168261ee
DzunN86 <nurroinz@gmail.com>
2020-08-14 23:49:21 +0700

    integrate API update

---
+++ api-gateway/routes/handler/users/update.js
@@ -0,0 +1,22 @@
+const apiAdapter = require('../../apiAdapter');
+const {
+  URL_SERVICE_USER
+} = process.env;
+
+const api = apiAdapter(URL_SERVICE_USER);
+
+module.exports = async (req, res) => {
+  try {
+    const id = req.user.data.id;
+    const user = await api.put(`/users/${id}`, req.body);
+    return res.json(user.data);
+  } catch (error) {
+
+    if (error.code === 'ECONNREFUSED') {
+      return res.status(500).json({ status: 'error', message: 'service unavailable' });
+    }
+
+    const { status, data } = error.response;
+    return res.status(status).json(data);
+  }
+}
--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const usersHandler = require('./handler/users');
 
+const verifyToken = require('../middlewares/verifyToken');
+
 /* GET users listing. */
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
+router.put('/', verifyToken, usersHandler.update);
 
 module.exports = router;
