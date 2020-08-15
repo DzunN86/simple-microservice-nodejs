1557198b4ae52c54ccc24d21cfcdbd6d1a75f08a
DzunN86 <nurroinz@gmail.com>
2020-08-15 13:21:49 +0700

    create tb chapters migration

---
+++ service-course/database/migrations/2020_08_15_061648_create_chapters.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChapters extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+}
