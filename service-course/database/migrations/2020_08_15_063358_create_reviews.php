feae7d39978264986ea17720397b0f59c0c48d32
DzunN86 <nurroinz@gmail.com>
2020-08-15 13:35:36 +0700

    create reviews migrations

---
+++ service-course/database/migrations/2020_08_15_063358_create_reviews.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->integer('rating')->default(1);
+            $table->longText('note')->nullable();
+            $table->unique(['course_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
