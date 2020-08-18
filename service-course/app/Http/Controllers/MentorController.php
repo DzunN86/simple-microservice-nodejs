bb21e2147e0aa6c332a4130527e413cd623a5619
DzunN86 <nurroinz@gmail.com>
2020-08-19 01:52:01 +0700

    api create mentors

---
+++ service-course/app/Http/Controllers/MentorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mentor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class MentorController extends Controller
+{
+    public function create(Request $request)
+    {
+        $rules = [
+            'name' => 'required|string',
+            'profile' => 'required|url',
+            'profession' => 'required|string',
+            'email' => 'required|email'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $mentor = Mentor::create($data);
+
+        return response()->json(['status' => 'success', 'data' => $mentor]);
+    }
+}
