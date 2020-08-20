f9bba4dea89e87a88caa3ad4524fb39a74d2cbdd
DzunN86 <nurroinz@gmail.com>
2020-08-20 21:20:36 +0700

    create route get list and detail mentors

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('mentors', 'MentorController@index');
+Route::get('mentors/{id}', 'MentorController@show');
 Route::post('mentors', 'MentorController@create');
 Route::put('mentors/{id}', 'MentorController@update');
