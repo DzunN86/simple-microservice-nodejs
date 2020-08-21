13194dff1f691c2d3783eb8b54625feeec4b1b52
DzunN86 <nurroinz@gmail.com>
2020-08-21 21:12:04 +0700

    create api update course

--- service-course/app/Http/Controllers/CourseController.php
+++ service-course/app/Http/Controllers/CourseController.php
@@ -52,4 +52,57 @@ class CourseController extends Controller
             'data' => $course
         ]);
     }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'string',
+            'certificate' => 'boolean',
+            'thumbnail' => 'string|url',
+            'type' => 'in:free,premium',
+            'status' => 'in:draft,published',
+            'price' => 'integer',
+            'level' => 'in:all-level,beginner,intermediate,advance',
+            'mentor_id' => 'integer',
+            'description' => 'string'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()){
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $course = Course::find($id);
+        if (!$course) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'course not found'
+            ], 404);
+        }
+
+        $mentorId = $request->input('mentor_id');
+        if ($mentorId) {
+            $mentor = Mentor::find($mentorId);
+            if (!$mentor) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'mentor not found'
+                ], 404);
+            }
+        }
+
+        $course->fill($data);
+        $course->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $course
+        ]);
+    }
 }
