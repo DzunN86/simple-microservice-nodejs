0d451c4296dceb770e3468bf33abd0df91ad5902
DzunN86 <nurroinz@gmail.com>
2020-08-24 19:42:59 +0700

    Create api update chapter

--- service-course/app/Http/Controllers/ChapterController.php
+++ service-course/app/Http/Controllers/ChapterController.php
@@ -3,8 +3,76 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Chapter;
+use App\Course;
+use Illuminate\Support\Facades\Validator;
 
 class ChapterController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $rules = [
+            'name' => 'required|string',
+            'course_id' => 'required|integer'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        
+    }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'string',
+            'course_id' => 'integer'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $chapter = Chapter::find($id);
+        if (!$chapter) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'chapter not found'
+            ], 404);
+        }
+
+        $courseId = $request->input('course_id');
+        if($courseId) {
+            $course = Course::find($courseId);
+            if (!$course) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'course not found'
+                ], 404);
+            }
+        }
+
+        $chapter->fill($data);
+        $chapter->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $chapter
+        ]);
+    }
 }
