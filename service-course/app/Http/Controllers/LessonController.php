0e6321743588a3ebfa194416685c4597bb18fae7
DzunN86 <nurroinz@gmail.com>
2020-08-29 18:15:53 +0700

    create api get list & detail lesson

--- service-course/app/Http/Controllers/LessonController.php
+++ service-course/app/Http/Controllers/LessonController.php
@@ -9,6 +9,39 @@ use Illuminate\Support\Facades\Validator;
 
 class LessonController extends Controller
 {
+
+    public function index(Request $request)
+    {
+        $lessons = Lesson::query();
+
+        $chapterId = $request->query('chapter_id');
+
+        $lessons->when($chapterId, function($query) use ($chapterId) {
+            return $query->where('chapter_id', '=', $chapterId);
+        });
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $lessons->get()
+        ]);
+    }
+
+    public function show($id)
+    {
+        $lesson = Lesson::find($id);
+        if (!$lesson) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'lesson not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $lesson
+        ]);
+    }
+
     public function create(Request $request)
     {
         $rules = [
