c83378bb59444c3aa8b259e185611269d70281af
DzunN86 <nurroinz@gmail.com>
2020-08-30 09:41:14 +0700

    api create image source

--- service-course/app/Http/Controllers/ImageSourceController.php
+++ service-course/app/Http/Controllers/ImageSourceController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class ImageSourceController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $rules = [
+            'image' => 'required|url',
+            'course_id' => 'required|integer'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ],400);
+        }
+
+        $courseId = $request->input('course_id');
+        $course = Course::find($courseId);
+        if (!$course) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'course not found'
+            ], 404);
+        }
+
+        $imageCourse = ImageCourse::create($data);
+        return response()->json([
+            'status' => 'success',
+            'data' => $imageCourse
+        ]);
+    }
 }
