75adf1d7cdda4829fe4301cd2a86ceec0d846bf4
DzunN86 <nurroinz@gmail.com>
2020-08-31 14:18:12 +0700

    create api review

--- service-course/app/Http/Controllers/ReviewController.php
+++ service-course/app/Http/Controllers/ReviewController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Course;
+use App\Review;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ReviewController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $rules = [
+            'user_id' => 'required|integer',
+            'course_id' => 'required|integer',
+            'rating' => 'required|integer|min:1|max:5',
+            'note' => 'string'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $courseId = $request->input('course_id');
+        $course = Course::find($courseId);
+        if (!$course) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'course not found'
+            ]);
+        }
+
+        $userId = $request->input('user_id');
+        $user = getUser($userId);
+
+        if ($user['status'] === 'error') {
+            return response()->json([
+                'status' => $user['status'],
+                'message' => $user['message']
+            ], $user['http_code']);
+        }
+
+        $isExistReview = Review::where('course_id', '=', $courseId)
+                                ->where('user_id', '=', $userId)
+                                ->exists();
+
+        if ($isExistReview) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'review already exist'
+            ], 409);
+        }
+
+        $review = Review::create($data);
+        return response()->json([
+            'status' => 'success',
+            'data' => $review
+        ]);
+    }
 }
