76baf6e481b71a1d9344656bc62c9cd75246d6a1
DzunN86 <nurroinz@gmail.com>
2020-09-02 23:05:07 +0700

    wkwkw salah naruh

--- api-gateway/routes/handler/media/destroy.js
+++ api-gateway/routes/handler/media/destroy.js
@@ -1,22 +1,23 @@
 const apiAdapter = require('../../apiAdapter');
-const {
-  URL_SERVICE_COURSE
-} = process.env;
 
-const api = apiAdapter(URL_SERVICE_COURSE);
+const{
+    URL_SERVICE_MEDIA
+} = process.env
 
-module.exports = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const mentor = await api.delete(`/api/mentors/${id}`);
-    return res.json(mentor.data);
-  } catch (error) {
+const api = apiAdapter(URL_SERVICE_MEDIA);
 
-    if (error.code === 'ECONNREFUSED') {
-      return res.status(500).json({ status: 'error', message: 'service unavailable' });
+module.exports = async(req, res) => {
+    try{
+        const id = req.params.id;
+        const media = await api.delete(`/media/${id}`);
+        return res.json(media.data);
+    }catch (error){
+
+        if(error.code === 'ECONNREFUSED'){
+            return res.status(500).json({ status: 'error', message: 'service unavailable'});
+        }
+
+        const { status, data } = error.response;
+        return res.status(status).json(data)
     }
-
-    const { status, data } = error.response;
-    return res.status(status).json(data);
-  }
 }
--- api-gateway/routes/handler/mentors/destroy.js
+++ api-gateway/routes/handler/mentors/destroy.js
@@ -1,23 +1,22 @@
 const apiAdapter = require('../../apiAdapter');
-
-const{
-    URL_SERVICE_COURSE
-} = process.env
+const {
+  URL_SERVICE_COURSE
+} = process.env;
 
 const api = apiAdapter(URL_SERVICE_COURSE);
 
-module.exports = async(req, res) => {
-    try{
-        const id = req.params.id;
-        const media = await api.delete(`/media/${id}`);
-        return res.json(media.data);
-    }catch (error){
+module.exports = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const mentor = await api.delete(`/api/mentors/${id}`);
+    return res.json(mentor.data);
+  } catch (error) {
 
-        if(error.code === 'ECONNREFUSED'){
-            return res.status(500).json({ status: 'error', message: 'service unavailable'});
-        }
-
-        const { status, data } = error.response;
-        return res.status(status).json(data)
+    if (error.code === 'ECONNREFUSED') {
+      return res.status(500).json({ status: 'error', message: 'service unavailable' });
     }
+
+    const { status, data } = error.response;
+    return res.status(status).json(data);
+  }
 }
