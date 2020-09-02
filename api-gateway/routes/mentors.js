b7db8173cffb84981d7bc8066df09a17dd6bee19
DzunN86 <nurroinz@gmail.com>
2020-09-02 22:56:33 +0700

    mento update

--- api-gateway/routes/mentors.js
+++ api-gateway/routes/mentors.js
@@ -7,6 +7,7 @@ const mentorsHandler = require('./handler/mentors');
 router.get('/', mentorsHandler.getAll);
 router.get('/:id', mentorsHandler.get);
 router.post('/', mentorsHandler.create);
+router.put('/:id', mentorsHandler.update);
 
 
 module.exports = router;
