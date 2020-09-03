03bc982fcc7b68cf5545d642e942ade4fe3018bd
DzunN86 <nurroinz@gmail.com>
2020-09-03 16:04:21 +0700

    add all api courses to router

--- api-gateway/routes/courses.js
+++ api-gateway/routes/courses.js
@@ -9,4 +9,8 @@ const verifyToken = require('../middlewares/verifyToken');
 router.get('/', coursesHandler.getAll);
 router.get('/:id', coursesHandler.get);
 
+router.post('/', verifyToken, coursesHandler.create);
+router.put('/:id', verifyToken, coursesHandler.update);
+router.delete('/:id', verifyToken, coursesHandler.destroy);
+
 module.exports = router;
