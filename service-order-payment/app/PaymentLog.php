bbe4eb9f1248bbec8d1bda4cb15efb6473ddfe07
DzunN86 <nurroinz@gmail.com>
2021-01-02 07:29:36 +0700

    update model payment log

--- service-order-payment/app/PaymentLog.php
+++ service-order-payment/app/PaymentLog.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaymentLog extends Model
 {
-    //
+    protected $table = 'payment_logs';
+
+    protected $fillabble = [
+        'status', 'payment_type', 'order_id', 'raw_response'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'update_at' => 'datetime:Y-m-d H:m:s',
+    ];
 }
