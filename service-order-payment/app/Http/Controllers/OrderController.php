2a72bd775f878cb1fbe266de309018b7d320e24c
DzunN86 <nurroinz@gmail.com>
2021-01-02 08:03:27 +0700

    get url midtrans

--- service-order-payment/app/Http/Controllers/OrderController.php
+++ service-order-payment/app/Http/Controllers/OrderController.php
@@ -10,6 +10,46 @@ class OrderController extends Controller
 
     public function create( Request $request) {
 
+        $user = $request->input('user');
+        $course = $request->input('course');
+
+        $order = Order::create([
+            'user_id' => $user['id'],
+            'course_id' => $course['id']
+        ]);
+        
+        $transactionDetails = [
+            'order_id' => $order->id,
+            'gross_amount' => $course['price']
+        ];
+
+        $itemDetails = [
+            [
+                'id' => $course['id'],
+                'price' => $course['price'],
+                'quality' => 1,
+                'name' => $course['name'],
+                'brand' => 'DzunClass',
+                'category' => 'Online Course'
+            ]
+            ];
+
+        $costumerDetails = [
+            'first_name' => $user['name'],
+            'email' => $user['email']
+        ];
+
+        $midtransParams = [
+            'transaction_details' => $transactionDetails,
+            'item_details' => $itemDetails,
+            'costumer_details' => $costumerDetails
+        ];
+
+        $midtransSnapUrl = $this->getMidtransSnapUrl($midtransParams);
+
+        return $midtransSnapUrl;
+        // return response()->json($order);
+
     }
 
     private function getMidtransSnapUrl($params){
