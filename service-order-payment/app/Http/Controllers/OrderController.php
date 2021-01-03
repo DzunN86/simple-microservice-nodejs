3c79abdd03d378311b3f310fffb3d5cac0ee178b
DzunN86 <nurroinz@gmail.com>
2021-01-03 15:12:18 +0700

    update

--- service-order-payment/app/Http/Controllers/OrderController.php
+++ service-order-payment/app/Http/Controllers/OrderController.php
@@ -8,20 +8,20 @@ use Illuminate\Support\Str;
 
 class OrderController extends Controller
 {
-    public function index(Request $request)
-    {
-        $userId = $request->input('user_id');
-        $orders = Order::query();
+    // public function index(Request $request)
+    // {
+    //     $userId = $request->input('user_id');
+    //     $orders = Order::query();
 
-        $orders->when($userId, function($query) use ($userId) {
-            return $query->where('user_id', '=', $userId);
-        });
+    //     $orders->when($userId, function($query) use ($userId) {
+    //         return $query->where('user_id', '=', $userId);
+    //     });
 
-        return response()->json([
-            'status' => 'success',
-            'data' => $orders->get()
-        ]);
-    }
+    //     return response()->json([
+    //         'status' => 'success',
+    //         'data' => $orders->get()
+    //     ]);
+    // }
 
     public function create(Request $request)
     {
@@ -44,7 +44,7 @@ class OrderController extends Controller
                 'price' => $course['price'],
                 'quantity' => 1,
                 'name' => $course['name'],
-                'brand' => 'BuildWithAngga',
+                'brand' => 'Dzun Class',
                 'category' => 'Online Course'
             ]
         ];
