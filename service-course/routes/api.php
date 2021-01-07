7f907a6dc10add3af44c0deb1133c5832b4a0dda
DzunN86 <nurroinz@gmail.com>
2021-01-07 23:20:11 +0700

    update route

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -43,6 +43,7 @@ Route::delete('image-courses/{id}', 'ImageCourseController@destroy');
 
 Route::post('my-courses', 'MyCourseController@create');
 Route::get('my-courses', 'MyCourseController@index');
+Route::post('my-courses/premium', 'MyCourseController@createPremiumAccess');
 
 Route::post('reviews', 'ReviewController@create');
 Route::put('reviews/{id}', 'ReviewController@update');
