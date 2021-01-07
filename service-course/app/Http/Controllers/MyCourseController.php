a0e7eb3142dd44e2ecb7b9f94e506e64909e83c6
DzunN86 <nurroinz@gmail.com>
2021-01-08 00:22:39 +0700

    Ubah Flow API MyCourse

--- service-course/app/Http/Controllers/MyCourseController.php
+++ service-course/app/Http/Controllers/MyCourseController.php
@@ -75,12 +75,41 @@ class MyCourseController extends Controller
             ],409);
         }
 
-        $myCourse = MyCourse::create($data);
 
-        return response()->json([
-            'status' => 'success',
-            'data' => $myCourse
-        ]);
+
+
+        if ($course->type === 'premium') {
+            if ($course->price === 0) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Price can\'t be 0'
+                ], 405);
+            }
+            
+            $order = postOrder([
+                'user' => $user['data'],
+                'course' => $course->toArray()
+            ]);
+
+            if ($order['status'] === 'error') {
+                return response()->json([
+                    'status' => $order['status'],
+                    'message' => $order['message']
+                ], $order['http_code']);
+            }
+
+            return response()->json([
+                'status' => $order['status'],
+                'data' => $order['data']
+            ]);
+        } else {
+            $myCourse = MyCourse::create($data);
+
+            return response()->json([
+                'status' => 'success',
+                'data' => $myCourse
+            ]);
+        }
     }
 
     public function createPremiumAccess(Request $request)
--- service-course/app/helpers.php
+++ service-course/app/helpers.php
@@ -43,3 +43,19 @@ function getUserByIds($userIds = []) {
        ];
     }
 }
+
+function postOrder($params) {
+    $url = env('SERVICE_ORDER_PAYMENT_URL').'api/orders';
+    try {
+        $response = Http::post($url, $params);
+        $data = $response->json();
+        $data['http_code'] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        return [
+            'status' => 'error',
+            'http_code' => 500,
+            'message' => 'service order payment unavailable'
+        ];
+    }
+}
